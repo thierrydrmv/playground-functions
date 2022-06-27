6a6b6d1fb641d275b3f965365b8035dabcb5696a
Thierry <varelathierry@gmail.com>
2022-06-27 15:02:45 -0300

    exercicio 3 transformando a string em um array

--- src/challenges.js
+++ src/challenges.js
@@ -15,8 +15,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  var array = string.split(" ");
+  return array
 }
 
 // Desafio 4

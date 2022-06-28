82beb82c9d200816850ec1d299a0d30c0c9b923b
Thierry <varelathierry@gmail.com>
2022-06-28 00:49:07 -0300

    exercicio 11 incompleto

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,16 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let arrayx = array.join("")
+  if (array.length > 11 || array.length < 11) {
+    return 'Array com tamanho incorreto.'
 }
-
+  for (let i = 0; i < array.length; i += 1) {
+  if (array[i] < 0 || array[i] > 9 ) {
+    return 'não é possível gerar um número de telefone com esses valores'
+  } 
+}
+return "(" + arrayx.slice(0,2) + ") " + arrayx.slice(2,7) + "-" + arrayx.slice(7-11)
+}
 // Desafio 12
 function triangleCheck() {
   // seu código aqui

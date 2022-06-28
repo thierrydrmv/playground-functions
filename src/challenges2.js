ce6cc45d454264aebe2d3fd842e5850d96616167
Thierry <varelathierry@gmail.com>
2022-06-28 01:10:14 -0300

    exercicio 11 gerando um número de telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,8 @@
 // Desafio 11
 function generatePhoneNumber(array) {
   let arrayx = array.join("")
+  let arrayy = []
+  let contador = 0
   if (array.length > 11 || array.length < 11) {
     return 'Array com tamanho incorreto.'
 }
@@ -8,7 +10,20 @@ function generatePhoneNumber(array) {
   if (array[i] < 0 || array[i] > 9 ) {
     return 'não é possível gerar um número de telefone com esses valores'
   } 
+  else{
+      for (let y = 0; y < array.length; y +=1) {
+      if (array[i] === array[y]) {
+      contador += 1
+      }
+    }
+    arrayy.push(contador)
+    contador = 0
+  }
+  if (arrayy[i]>=3) {
+    return 'não é possível gerar um número de telefone com esses valores'
+  }
 }
+
 return "(" + arrayx.slice(0,2) + ") " + arrayx.slice(2,7) + "-" + arrayx.slice(7-11)
 }
 // Desafio 12
